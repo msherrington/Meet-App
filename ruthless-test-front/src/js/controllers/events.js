c29b2eaa681a5edcc872fd7881aad6576e0127c9
Conor <conor.heena2@mail.dcu.ie>
2017-04-18 15:18:11 +0100

    eventsnew working, attendees working, comments working. Still issues with ng-if

--- ruthless-test-front/src/js/controllers/events.js
+++ ruthless-test-front/src/js/controllers/events.js
@@ -31,27 +31,28 @@ function EventsNewCtrl(Event, User, $state) {
   vm.create = eventsCreate;
 }
 
-// EventsNewCtrl.$inject = ['Event', '$state'];
-// function EventsNewCtrl(Event, $state){
-  // const vm = this;
 
-  // vm.countries = Event.query();
-
-//   function submit(){
-//     Event.save(vm.event)
-//       .$promise
+// EventsShowCtrl.$inject = ['Event', '$stateParams', '$state']
+// function EventsShowCtrl(Event, $stateParams, $state){
+//   const vm = this;
+//
+//   vm.event = Event.get($stateParams);
+//   console.log(vm.event)
+//
+//   function eventsDelete() {
+//     vm.event
+//       .$remove()
 //       .then(() => $state.go('eventsIndex'));
 //   }
 //
-//   vm.submit = submit;
+//   vm.delete = eventsDelete;
 // }
-
-EventsShowCtrl.$inject = ['Event', '$stateParams', '$state']
-function EventsShowCtrl(Event, $stateParams, $state){
+EventsShowCtrl.$inject = ['Event', 'User', 'Comment', '$stateParams', '$state', '$auth'];
+function EventsShowCtrl(Event, User, Comment, $stateParams, $state, $auth) {
   const vm = this;
+  if ($auth.getPayload()) vm.currentUser = User.get({ id: $auth.getPayload().id });
 
   vm.event = Event.get($stateParams);
-  console.log(vm.event)
 
   function eventsDelete() {
     vm.event
@@ -60,7 +61,60 @@ function EventsShowCtrl(Event, $stateParams, $state){
   }
 
   vm.delete = eventsDelete;
+
+  function eventsUpdate() {
+    Event
+      .update({id: vm.event.id, event: vm.event });
+  }
+
+  function addComment() {
+    vm.comment.event_id = vm.event.id;
+
+    Comment
+      .save({ comment: vm.comment })
+      .$promise
+      .then((comment) => {
+        vm.event.comments.push(comment);
+        vm.comment = {};
+      });
+  }
+
+  vm.addComment = addComment;
+
+  function deleteComment(comment) {
+    Comment
+      .delete({ id: comment.id })
+      .$promise
+      .then(() => {
+        const index = vm.event.comments.indexOf(comment);
+        vm.event.comments.splice(index, 1);
+      });
+  }
+
+  vm.deleteComment = deleteComment;
+
+  // function toggleAttending() {
+  //   const index = vm.event.attendee_ids.indexOf(vm.currentUser.id);
+  //   if (index > -1) {
+  //     vm.event.attendee_ids.splice(index, 1);
+  //     vm.event.attendees.splice(index, 1);
+  //   } else {
+  //     vm.event.attendee_ids.push(vm.currentUser.id);
+  //     vm.event.attendees.push(vm.currentUser);
+  //   }
+  //   eventsUpdate();
+  // }
+  //
+  // vm.toggleAttending = toggleAttending;
+  //
+  // function isAttending() {
+  //   return $auth.getPayload() && vm.event.$resolved && vm.event.attendee_ids.includes(vm.currentUser.id);
+  // }
+  //
+  // vm.isAttending = isAttending;
 }
+
+
 
 EventsEditCtrl.$inject = ['Event', '$stateParams', '$state'];
 function EventsEditCtrl(Event, $stateParams, $state) {
