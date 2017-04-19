10f3186ed764df30df681f42171255c09de5ae28
Mark Sherrington <thewayforward3000@gmail.com>
2017-04-19 10:23:56 +0100

    Google places location autocomplete working and lat long added to Event model

--- ruthless-test-front/src/js/factories/event.js
+++ ruthless-test-front/src/js/factories/event.js
@@ -2,7 +2,7 @@ angular
   .module('ruthless-test-front')
   .factory('Event', Event);
 
-Event.$inject = ['$resource', 'API_URL']
+Event.$inject = ['$resource', 'API_URL'];
 function Event($resource, API_URL){
   return new $resource(`${API_URL}/events/:id`, { id: '@id' }, {
     update: { method: 'PUT' }
