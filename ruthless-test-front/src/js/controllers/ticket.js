507ff2ba3fd7b2590d458ed07a19f0b97aea1dc2
Conor <conor.heena2@mail.dcu.ie>
2017-04-21 14:32:54 +0100

    can delete events with comments and delete tickets and unattend

--- ruthless-test-front/src/js/controllers/ticket.js
+++ ruthless-test-front/src/js/controllers/ticket.js
@@ -7,20 +7,20 @@ function TicketsConfirmCtrl(Event, User, Ticket, $stateParams, $auth, $state) {
   const vm = this;
   vm.ticket = {};
   vm.event = Event.get($stateParams);
-  vm.currentUserId = $auth.getPayload();
+  vm.currentUserId = $auth.getPayload().id;
 
   //Pull in tickets from factory
-  Ticket.query()
-    .$promise
-    .then((tickets)=>{
-      vm.all = tickets;
-      console.log(tickets);
-    });
+  // Ticket.query()
+  //   .$promise
+  //   .then((tickets)=>{
+  //     vm.all = tickets;
+  //     console.log(tickets);
+  //   });
 
   // console.log('eventId: ', $stateParams.id);
   // console.log('userId: ', $auth.getPayload().id);
 
-  // console.log(vm.tickets)
+  console.log(vm.ticket)
 
   function ticketCreate() {
     vm.ticket.event_id = vm.event.id;
@@ -35,59 +35,14 @@ function TicketsConfirmCtrl(Event, User, Ticket, $stateParams, $auth, $state) {
   // console.log(vm.ticket)
 
   function ticketDelete() {
+    const ticket = vm.event.tickets.find((ticket) => {
+      return ticket.user.id === vm.currentUserId;
+    });
 
-      //Function to find id of ticket that matches user id
-      var attendeesArray = [];
-      for(var i =0; i< vm.event.tickets.length; i++){
-        // console.log(vm.event.tickets[i].user.id);
-        // console.log(vm.event.tickets[i].user);
-        attendeesArray.push(vm.event.tickets[i].user.id);
-      }
-      for(i = 0; i < attendeesArray.length; i++) {
-        // console.log(attendeesArray[i]);
-        // console.log(vm.currentUserId);
-        if(vm.currentUserId.id === attendeesArray[i]){
-          console.log('Ticket Found!');
-          console.log(attendeesArray[i]);
-          var ticketIndex = i;
-        } else{
-          console.log('not your ticket!');
-        }
-      }
-      console.log(ticketIndex + ' TICKET INDEX ')
-      console.log(vm.all[ticketIndex]);
-      const ticket = vm.all[ticketIndex];
-      Ticket
-          .delete({ id: ticket.id, ticket: ticket })
-          .$promise
-          .then(() => {
-            const index = vm.event.tickets[ticketIndex];
-            vm.event.tickets.splice(index, 1);
-            $state.go('eventsIndex');
-          });
-
-    //   function deleteComment(comment) {
-    //     Comment
-    //     .delete({ id: comment.id })
-    // .$promise
-    // .then(() => {
-    //   const index = vm.event.comments.indexOf(comment);
-    //   vm.event.comments.splice(index, 1);
-    // });
-// }
-
-      console.log(vm.event.tickets[ticketIndex] + " To be deleted")
-      console.log(vm.event.tickets[ticketIndex].id)
-      console.log(vm.event.tickets[ticketIndex].user)
-      // console.log(vm.event.tickets[ticketIndex].event_id)
-
-      // vm.ticket
-        // .$remove({ id: vm.ticket.id } )
-      //   .$remove()
-      //   .$promise
-      //   .then(() => {
-          // $state.go('eventsUnattend');
-      //   });
-    }
-    vm.delete = ticketDelete;
+    Ticket
+      .remove({ id: ticket.id })
+      .$promise
+      .then(() => $state.go('eventsUnattend'));
+  }
+  vm.delete = ticketDelete;
 }
