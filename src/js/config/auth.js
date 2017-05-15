f2f4761fa2b94d7c6bdac7d8e3213d77ac383b70
Mark Sherrington <thewayforward3000@gmail.com>
2017-05-15 19:48:04 +0100

    updated github and facebook clientIds

--- src/js/config/auth.js
+++ src/js/config/auth.js
@@ -8,11 +8,11 @@ function Auth($authProvider, API_URL) {
   $authProvider.loginUrl = `${API_URL}/login`;
 
   $authProvider.github({
-    clientId: '22feb4eb3c5b5fa23e1d',
+    clientId: 'd22c7df7a284b74ea5c8',
     url: `${API_URL}/oauth/github`
   });
   $authProvider.facebook({
-    clientId: '285230671929861',
+    clientId: '680991948746989',
     url: `${API_URL}/auth/facebook`
   });
 }
